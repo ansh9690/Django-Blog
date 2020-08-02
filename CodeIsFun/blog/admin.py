d265ec5a8e8c80160cb8df1bbe4809b0898fe3fc
Anshu Prakash <anshuprakash8445@gmail.com>
2020-08-02 16:20:38 +0530

    Update

--- CodeIsFun/blog/admin.py
+++ CodeIsFun/blog/admin.py
@@ -3,11 +3,13 @@ from .models import Post, BlogComment
 
 admin.site.register(BlogComment)
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'sno', 'author','timeStamp')
+    list_display = ('title', 'author', 'timeStamp')
     list_filter = ("title",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'sno': ('title',)}
+
 
 admin.site.register(Post, PostAdmin)
 
